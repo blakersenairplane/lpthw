82110f15d34198483484c3ca04dc09df6c7fb48b
Blake Jensen <blakersen@gmail.com>
2017-12-12 10:56:36 -0700

    ex22 / 23

---
+++ ex23.py
@@ -0,0 +1,23 @@
+import sys
+script, input_encoding, error = sys.argv
+
+
+def main(lanague_file, encoding, errors):
+    line = language_file.readline()
+
+    if line:
+        print_line(line, encoding, errors)
+        return main(lanague_file, encoding, errors)
+
+
+def print_line(line, encoding, errors):
+    next_lang = line.strip() #returns a copy of the string with leading characters removed if set to no string it will remove whitespace
+    raw_bytes = next_lang.encode(encoding, errors=errors)
+    cooked_string = raw_bytes.decode(encoding, errors=errors)
+
+    print(raw_bytes, "<===>", cooked_string)
+
+
+languages = open("languages.txt", encoding="utf-8")
+
+main(languages, input_encoding, error)
