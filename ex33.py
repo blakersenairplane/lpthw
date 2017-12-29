32b0cdbd7db2cf37d473e741cc41ed7d321e2ee7
Jensen <blakersen@gmail.com>
2017-12-28 18:14:56 -0700

    New lessons with loops etc

---
+++ ex33.py
@@ -0,0 +1,33 @@
+i = 0
+numbers = []
+incremental = input("> ")
+
+
+def whileLoop(number, increment):
+    while number < 6:
+        print(f"At the top of the i is {i}")
+        numbers.append(number)
+
+        number = number + increment
+
+        print("Numbers now: ", numbers)
+        print(f"At the bottom of i is {i}")
+
+# while i < 6:
+#     print(f"At the top of i is {i}")
+#     numbers.append(i)
+#
+#
+#     i = i + 1
+#
+#     print("Numbers now:", numbers)
+#     print(f"At the bottom of i is {i}")
+#
+
+whileLoop(i, incremental)
+print("The numbers: ")
+
+for num in numbers:
+    print(num)
+
+
